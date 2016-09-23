76e716cf14c62a1d5229e7eff3c28c40ba5fc450
Kendall E. Blake <kendallb@kendallblake.com>
2016-09-23 00:33:52 -0400

    Fix up licensing

--- hello.c
+++ hello.c
@@ -1,20 +1,20 @@
 /*
  *  Copyright 2016 Kendall E. Blake
  *
- *  This file is part of This program.
+ *  This file is part of cairo-test.
  *
- *  This program is free software: you can redistribute it and/or modify
+ *  cairo-test is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published
  *  by the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  cairo-test is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with This program.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with cairo-test.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <SDL.h>
